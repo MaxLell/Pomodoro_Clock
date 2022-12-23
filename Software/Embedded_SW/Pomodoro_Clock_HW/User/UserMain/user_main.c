b2a9658e68bc6cbb64b9c73a897e6dab6d03ea28
Max Lell <maximilian.lell@gmail.com>
2022-12-23 08:47:13 +0100

    Initial Commit

---
+++ Software/Embedded_SW/Pomodoro_Clock_HW/User/UserMain/user_main.c
@@ -0,0 +1,19 @@
+/*
+ * user_main.c
+ *
+ *  Created on: Dec 23, 2022
+ *      Author: maxlell
+ */
+
+#include "user_main.h"
+
+void user_main()
+{
+	/* Setup */
+
+	/* Loop */
+	for (;;)
+	{
+		// do stuff
+	}
+}
---
+++ Software/Embedded_SW/Pomodoro_Clock_HW/User/UserMain/user_main.h
@@ -0,0 +1,21 @@
+/*
+ * user_main.h
+ *
+ *  Created on: Dec 23, 2022
+ *      Author: maxlell
+ */
+
+#ifndef USERMAIN_USER_MAIN_H_
+#define USERMAIN_USER_MAIN_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void user_main(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* USERMAIN_USER_MAIN_H_ */
