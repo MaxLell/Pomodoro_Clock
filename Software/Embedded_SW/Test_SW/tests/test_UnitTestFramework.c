41df2d9742377990abd38ddb033062bc78261a07
Max Lell <maximilian.lell@gmail.com>
2022-12-23 09:56:04 +0100

    Added the debug makro tool to the project, to simplify future code development

--- Software/Embedded_SW/Test_SW/tests/test_UnitTestFramework.c
+++ Software/Embedded_SW/Test_SW/tests/test_UnitTestFramework.c
@@ -1,13 +1,38 @@
+/*
+ * Filename: /Users/maxlell/Documents/GitHub/Pomodoro_Clock/Software/Embedded_SW/Test_SW/tests/test_UnitTestFramework.c
+ * Path: /Users/maxlell/Documents/GitHub/Pomodoro_Clock/Software/Embedded_SW/Test_SW/tests
+ * Created Date: Friday, December 23rd 2022, 9:50:25 am
+ * Author: Max Lell
+ *
+ * Copyright (c) 2022 Your Company
+ */
 
 #include <stdlib.h>
+
+#include "../../Pomodoro_Clock_HW/User/Common/dbg.h"
+/*
+Author: Hello (World)
+test_UnitTestFramework.c (c) 2022
+Desc: description
+Created:  2022-12-23T08:55:08.162Z
+Modified: !date!
+*/
 
 #include "../../Pomodoro_Clock_HW/User/UnityTest/add.h"
 #include "unity_lib/unity.h"
 
-void test_can_cubeide_file_be_tested() {
+void test_can_cubeide_file_be_tested()
+{
   int a = 1;
   int b = 2;
   int c = 3;
+
+  /* Run the dummy function that is in the CubeIDE Project */
   int d = add(a, b);
+
+  /* check whether the debug makro has been loaded properly */
+  log_info("Assert: %d", (c == d));
+
+  /* Check whether the Unity Command can be executed properly */
   TEST_ASSERT(c == d);
 }
--- Software/Embedded_SW/Test_SW/tests/test_runner.c
+++ Software/Embedded_SW/Test_SW/tests/test_runner.c
@@ -1,14 +1,24 @@
+/*
+Author: Hello (World)
+test_runner.c (c) 2022
+Desc: description
+Created:  2022-12-23T08:54:01.460Z
+Modified: !date!
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "unity_lib/unity.h"
 
-void setUp(void) {
+void setUp(void)
+{
   // MANDATORY
   // set stuff up here
 }
 
-void tearDown(void) {
+void tearDown(void)
+{
   // MANDATORY
   // clean stuff up here
 }
@@ -18,7 +28,8 @@ extern void test_can_cubeide_file_be_tested();
 /**
  * Test Runner
  */
-int main(void) {
+int main(void)
+{
   UNITY_BEGIN();
 
   /* Tool chain test */
