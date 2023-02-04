c614cdb234324d02f3c1550f327f005f81d7f64b
Max Lell <maximilian.lell@gmail.com>
2023-02-04 06:05:40 +0000

    Managed to get a simply blinky LED

--- Software/Firmware/L476_Pomodoro_App/App/Main/Main.c
+++ Software/Firmware/L476_Pomodoro_App/App/Main/Main.c
@@ -1,6 +1,10 @@
 #include "Main.h"
 #include "Common.h"
 #include "Rcc.h"
+#include "stm32l4xx.h"
+
+/* delay counter */
+#define DELAY_MAX 50000
 
 #ifdef TEST
 int TestableMain(void)
@@ -10,11 +14,23 @@ int main(void)
 {
     printf("huhu\n");
 
-    Rcc_Init();
+    /* Turn on the Clock for GPIOA */
+    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
 
+    /* Set PA5 to output Mode */
+    GPIOA->MODER &= ~GPIO_MODER_MODE5_1;
+    GPIOA->MODER |= GPIO_MODER_MODE5_0;
 
     while (TRUE)
     {
-        /* Do nuthin */
+        /* set HIGH value on pin PA5 */
+        GPIOA->BSRR |= GPIO_BSRR_BS_5;
+        for (uint32_t i = DELAY_MAX; i--;)
+            ;
+
+        /* set LOW value on pin PA5 */
+        GPIOA->BSRR |= GPIO_BSRR_BR_5;
+        for (uint32_t i = DELAY_MAX; i--;)
+            ;
     }
 }
