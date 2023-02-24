705ef5fb18446b3e9d568669fec60e14bab4b3b7
Max Lell <maximilian.lell@gmail.com>
2023-02-24 16:19:15 +0000

    Added a test CI script, that checks against the Misra Coding standard + a Code Linter that checks agains the google coding standard

--- Software/Firmware/L476_Pomodoro_App/App/Executer/Executer.c
+++ Software/Firmware/L476_Pomodoro_App/App/Executer/Executer.c
@@ -1,49 +1,44 @@
 #include "Executer.h"
 
-#include "Rcc.h"
 #include "BlinkyLed.h"
-#include "SysTick.h"
 #include "Button_Conductor.h"
 #include "MessageBroker.h"
+#include "Rcc.h"
+#include "SysTick.h"
 
 status_t tDummyTriggerCallback(MessageBroker_message_t);
 BOOL bTriggered = FALSE;
 
-void Executer_init(void)
-{
-    Rcc_init();
-    SysTick_init();
-    BlinkyLed_init();
-    Button_Conductor_init();
-    MessageBroker_init();
+void Executer_init(void) {
+  Rcc_init();
+  SysTick_init();
+  BlinkyLed_init();
+  Button_Conductor_init();
+  MessageBroker_init();
 
-    /**
-     * Register the current dummy function as being the
-     * Callback function of the Button Trigger
-     */
-    MessageBroker_subscribe(E_TRIGGER_BUTTON_EVENT, tDummyTriggerCallback);
+  /**
+   * Register the current dummy function as being the
+   * Callback function of the Button Trigger
+   */
+  MessageBroker_subscribe(E_TRIGGER_BUTTON_EVENT, tDummyTriggerCallback);
 }
 
-status_t Executer_exec(void)
-{
+status_t Executer_exec(void) {
+  /**
+   * Run the Button Conductor
+   * This polls on the ISR output.
+   */
+  Button_Conductor_exec();
 
-    /**
-     * Run the Button Conductor
-     * This polls on the ISR output.
-     */
-    Button_Conductor_exec();
+  if (bTriggered == TRUE) {
+    bTriggered = FALSE;
+    BlinkyLed_toggle();
+  }
 
-    if (bTriggered == TRUE)
-    {
-        bTriggered = FALSE;
-        BlinkyLed_toggle();
-    }
-
-    return STATUS_OK;
+  return STATUS_OK;
 }
 
-status_t tDummyTriggerCallback(MessageBroker_message_t tDummyMessage)
-{
-    bTriggered = TRUE;
-    return STATUS_OK;
+status_t tDummyTriggerCallback(MessageBroker_message_t tDummyMessage) {
+  bTriggered = TRUE;
+  return STATUS_OK;
 }
