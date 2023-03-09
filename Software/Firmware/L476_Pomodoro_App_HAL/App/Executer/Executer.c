515c91f3f358aae663d003f6fc26c20a0c7b96d2
Max Lell <maximilian.lell@gmail.com>
2023-03-09 21:11:33 +0000

    Got the RTC up and running. The current time and date can be set. Upon a Reset the current time is maintained. No alars will be needed

--- Software/Firmware/L476_Pomodoro_App_HAL/App/Executer/Executer.c
+++ Software/Firmware/L476_Pomodoro_App_HAL/App/Executer/Executer.c
@@ -4,37 +4,6 @@
 #include "Button_Conductor.h"
 #include "MessageBroker.h"
 
-status_t tDummyTriggerCallback(MessageBroker_message_t);
-BOOL bTriggered = FALSE;
+void Executer_init(void) { BlinkyLed_init(); }
 
-void Executer_init(void) {
-  BlinkyLed_init();
-  Button_Conductor_init();
-  MessageBroker_init();
-
-  /**
-   * Register the current dummy function as being the
-   * Callback function of the Button Trigger
-   */
-  MessageBroker_subscribe(E_TRIGGER_BUTTON_EVENT, tDummyTriggerCallback);
-}
-
-status_t Executer_exec(void) {
-  /**
-   * Run the Button Conductor
-   * This polls on the ISR output.
-   */
-  Button_Conductor_exec();
-
-  if (bTriggered == TRUE) {
-    bTriggered = FALSE;
-    BlinkyLed_toggle();
-  }
-
-  return STATUS_OK;
-}
-
-status_t tDummyTriggerCallback(MessageBroker_message_t tDummyMessage) {
-  bTriggered = TRUE;
-  return STATUS_OK;
-}
+status_t Executer_exec(void) { return STATUS_OK; }
