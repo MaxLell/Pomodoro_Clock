342b2f321db6683a8dea06ebcfa8b9acc352dfe6
Max Lell <maximilian.lell@gmail.com>
2023-12-23 18:38:11 +0100

    Update LightEffects_Datastructures.h

--- Software/Firmware/G071_Pomodoro_Prototype/User/LightEffects/LightEffects_Datastructures.h
+++ Software/Firmware/G071_Pomodoro_Prototype/User/LightEffects/LightEffects_Datastructures.h
@@ -3,7 +3,7 @@
 
 #include "Common.h"
 
-#define MAX_SETTINGS 4
+#define MAX_SETTINGS 3U
 
 typedef enum {
   E_INNER_RING = 1U,
@@ -14,8 +14,8 @@ typedef enum {
 typedef enum {
   E_ANIMATION_WORK_TIME_ = 1U,
   E_ANIMATION_BREAK_TIME,
-  E_ANIMATION_WARNING,
-  E_ANIMATION_HIGH_BRIGHTNESS,
+  E_ANIMATION_FLASHLIGHT,
+  E_ANIMATION_BREAK_TIME_BRIGHT,
   E_ANIMATION_NOT_DEFINED
 } AnimationType_e;
 
@@ -24,11 +24,11 @@ typedef struct {
   AnimationType_e sAnimationType;
   uint8_t u8NumberOfElements;
   uint8_t u8Offset;
-} RingConfig_t;
+} RingPhaseConfig_t;
 
 typedef struct {
-  RingConfig_t tWorktimeConfig[MAX_SETTINGS];
-  RingConfig_t tBreaktimeConfig[MAX_SETTINGS];
+  RingPhaseConfig_t tWorktimeRingPhaseConfig[MAX_SETTINGS];
+  RingPhaseConfig_t tBreaktimeRingPhaseConfig[MAX_SETTINGS];
 } RingsDisplayConfig_t;
 
 #endif  // LIGHTEFFECTS_DATASTRUCTURES_H
